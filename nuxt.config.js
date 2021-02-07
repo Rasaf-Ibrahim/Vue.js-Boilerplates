d89336ffc831678dfd2222fd6c457d1574616dfd
Rasaf Ibrahim <69809655+Rasaf-Ibrahim@users.noreply.github.com>
2021-02-07 14:12:38 +0600

    updated

---
+++ nuxt.config.js
@@ -0,0 +1,52 @@
+
+export default {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: true,
+
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'server',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'nuxt-scss boilerplate',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [ '~/assets/styles/_index.scss'],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    '@nuxtjs/style-resources'
+  ],
+
+  styleResources: {
+    scss: ['~/assets/styles/_index.scss'],
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  },
+}
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>nuxt-scss boilerplate</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+h1{
+  background:$background;
+}
+</style>
