e88c8bce0211200a1bf85319f507d708f199ba16
Rasaf Ibrahim <69809655+Rasaf-Ibrahim@users.noreply.github.com>
2021-02-13 23:59:02 +0600

    updated

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="wrapper">
     <p class=para-1>This is the nuxt-scss boilerplate.</p>
     <p class="para-2">After installing nuxt, run 'npm install sass sass-loader@10 fibers --save-dev' so that you can use sass. You can get this command from nuxt official website. Just check before you run this command so that you can change the command if the official document have changed it.</p>
     <p class="para-3"> We will just run this command, nothing more. We will not edit our nuxt.config.js and so we have to import the sass file loacation everytime when we want to use that in a component or page. But I guess that's not a hard work to do. Check the style section of the index.vue page. You will see that I have just imported one scss file. Actually other scss file are imported on that one main file. 
@@ -18,6 +18,11 @@
 <style lang="scss" scoped>
 
 @import '~/assets/scss/style.scss';
+
+.wrapper{
+  @include flex-center(column);
+}
+
 
 .para-1{
   background:$red;
