63e6ce45e525680e05c7095c1f4fc15e107230bf
Rasaf Ibrahim <69809655+Rasaf-Ibrahim@users.noreply.github.com>
2021-03-02 15:40:58 +0600

    updated

--- components/navbar-bootstrap.vue
+++ components/navbar-bootstrap.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -39,13 +39,13 @@
 </nav>
 
 
+
     </div>
 </template>
 
 <script>
     export default {
         
-
     }
 </script>
 
