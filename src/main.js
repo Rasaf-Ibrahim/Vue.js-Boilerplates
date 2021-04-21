56d8539482998b03f79311b633d0f18769785832
Rasaf-Ibrahim <rasaf.ibrahim25@gmail.com>
2021-04-21 16:35:06 +0600

    updated

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './index.css'
+import '../src/tailwind css/index.css'
 
 createApp(App).use(store).use(router).mount('#app')
