fb5ce3b2c1d94f451dfc3afcc3bb4f63f30601f0
Rasaf-Ibrahim <rasaf.ibrahim25@gmail.com>
2021-06-19 11:11:15 +0600

    updated

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="text-xl">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
+</template>
+
+
---
+++ src/pages/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-gray-300 text-4xl text-center py-20">
+    This is a Vue, SCSS, Tailwind CSS Boilerplate.
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+
+</style>
